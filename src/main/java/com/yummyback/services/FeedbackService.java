e6e1edb7d91e6df7307375e1329fca30df8c437b
pushpithaDilhan <pushpitha.14@cse.mrt.ac.lk>
2017-11-12 19:34:21 +0530

    removed unused imports

--- src/main/java/com/yummyback/services/FeedbackService.java
+++ src/main/java/com/yummyback/services/FeedbackService.java
@@ -2,21 +2,13 @@ package com.yummyback.services;
 
 import com.yummyback.models.Feedback;
 import com.yummyback.util.MybatisUtil;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.Reader;
 
 public class FeedbackService {
 
     public void InsertFeedback(Feedback feedback) throws IOException {
-        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        SqlSession session24 = sqlSessionFactory.openSession();
-
         SqlSession session = MybatisUtil.getSqlSessionFactory().openSession();
         session.insert("Feedback.insert", feedback);
         session.commit();
@@ -25,7 +17,7 @@ public class FeedbackService {
 
     public Feedback GetFeedbackByUUID(String UUID) throws IOException {
         SqlSession session = MybatisUtil.getSqlSessionFactory().openSession();
-        Feedback feedback = (Feedback) session.selectOne("Feedback.getByUUID", UUID);
+        Feedback feedback = session.selectOne("Feedback.getByUUID", UUID);
         session.commit();
         session.close();
         return feedback;
