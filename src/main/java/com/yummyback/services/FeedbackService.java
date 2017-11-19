3776bfc91f9d2757b979cd55a4af291ae226e77e
PushpithaDilhan <pushpitha.14@cse.mrt.ac.lk>
2017-11-19 22:53:47 +0530

    list all feedbacks

--- src/main/java/com/yummyback/services/FeedbackService.java
+++ src/main/java/com/yummyback/services/FeedbackService.java
@@ -5,8 +5,21 @@ import com.yummyback.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
 import java.io.IOException;
+import java.util.List;
 
 public class FeedbackService {
+
+    private static FeedbackService feedbackService;
+
+    static {
+        if(feedbackService == null){
+                feedbackService = new FeedbackService();
+        }
+    }
+
+    public static FeedbackService getFeedbackService(){
+        return feedbackService;
+    }
 
     public void InsertFeedback(Feedback feedback) throws IOException {
         SqlSession session = MybatisUtil.getSqlSessionFactory().openSession();
@@ -21,6 +34,14 @@ public class FeedbackService {
         session.commit();
         session.close();
         return feedback;
+    }
+
+    public List<Feedback> GetAllFeedbacks(){
+        SqlSession session = MybatisUtil.getSqlSessionFactory().openSession();
+        List<Feedback> all_feedbacks = session.selectList("Feedback.getAllFeedbacks");
+        session.commit();
+        session.close();
+        return all_feedbacks;
     }
 
 }
